95a0e1db89d3bc84185659db2f6a921df700a5bf
MaBadiDen <kaba4ok.den@gmail.com>
2022-11-23 01:39:31 +0300

    Initial commit

---
+++ src/main/java/com/example/bask/BaskApplication.java
@@ -0,0 +1,13 @@
+package com.example.bask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/bask/BaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
